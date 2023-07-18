94f172e266657ede76c4a1f175a7846cc4184bf2
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-07-18 19:36:58 +0700

    CRUD UserAPI

---
+++ Project_API/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Project_API.DTO;
+using Project_API.Models;
+
+namespace Project_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private PRN_ProjectContext _context;
+
+        public UserController(PRN_ProjectContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("GetUserById/{userId}")]
+        public async Task<User> GetUserById(int userId)
+        {
+            var existuser = _context.Users.FirstOrDefault(x => x.UserId == userId);
+
+            return existuser;
+
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult CreateNewUser(UserDTO user)
+        {
+            try
+            {
+                if (user == null)
+                {
+                    return BadRequest();
+                }
+                var newuser = new User
+                {
+                    Username = user.Username,
+                    Password = user.Password,
+                    Email = user.Email, 
+                    Role = user.Role,
+                    Description = user.Description,
+                    Img = user.Img
+                };
+                _context.Users.Add(newuser);
+                var rowAffected = _context.SaveChanges();
+                return Ok(rowAffected);
+
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex);
+            }
+
+            
+        }
+
+        [HttpPut("{userId}")]
+        public IActionResult EditUser(int userId, UserDTO user)
+        {
+            try
+            {
+                var existuser = _context.Users.FirstOrDefault(x => x.UserId == userId);
+                if(existuser == null || user == null)
+                {
+                    return NotFound("Does not have account! Plz create new account!");
+                }
+
+                existuser.Username = user.Username;
+                existuser.Password = user.Password;
+                existuser.Email = user.Email;
+                existuser.Role = user.Role;
+                existuser.Description = user.Description;
+                existuser.Img = user.Img;
+
+                 _context.Users.Update(existuser);
+                var rowAffected = _context.SaveChanges();
+                return Ok(rowAffected);
+
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex);
+            }
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DleteteUser(int userId)
+        {
+            try
+            {
+                var existuser = _context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (existuser == null)
+                {
+                    return NotFound("Does not have account! Plz create new account!");
+                }
+                _context.Users.Remove(existuser);
+                var rowAffected = _context.SaveChanges();
+                return Ok(rowAffected);
+
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex);
+            }
+        }
+    }
+}
